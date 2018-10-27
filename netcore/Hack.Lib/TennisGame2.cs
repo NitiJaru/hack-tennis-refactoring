367306315317912e8cd2c11165f0d084d9bedc6d
Earn <earndarkness@perfenterprise.com>
2018-10-27 15:38:02 +0700

    clean code v2

--- netcore/Hack.Lib/TennisGame2.cs
+++ netcore/Hack.Lib/TennisGame2.cs
@@ -7,102 +7,48 @@ namespace Hack.Lib
         private int player1point;
         private int player2point;
 
-        private string p1res;
-        private string p2res;
+        private string player1;
+        private string player2;
         private string player1Name;
         private string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
+            player1Name = player1Name;
             this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
             var score = "";
-            if (player1point == player2point && player1point < 3)
+            if (player1point == player2point)
             {
-                if (player1point == 0)
-                    score = "Love";
-                if (player1point == 1)
-                    score = "Fifteen";
-                if (player1point == 2)
-                    score = "Thirty";
-                score += "-All";
+                if (player1point < 3) score = GetDisplayText(player1point) + "-All";
+                else score = "Deuce";
             }
-            if (player1point == player2point && player1point > 2)
-                score = "Deuce";
-
-            if (player1point > 0 && player2point == 0)
+            else if (player1point > 3 || player2point > 3)
             {
-                if (player1point == 1)
-                    p1res = "Fifteen";
-                if (player1point == 2)
-                    p1res = "Thirty";
-                if (player1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
+                var winner = (player1point > player2point) ? "player1" : "player2";
+                var isWin = (Math.Abs(player1point - player2point) >= 2)? "Win for ": "Advantage ";
+                score = isWin + winner;
             }
-            if (player2point > 0 && player1point == 0)
+            else
             {
-                if (player2point == 1)
-                    p2res = "Fifteen";
-                if (player2point == 2)
-                    p2res = "Thirty";
-                if (player2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
-            }
-
-            if (player1point > player2point && player1point < 4)
-            {
-                if (player1point == 2)
-                    p1res = "Thirty";
-                if (player1point == 3)
-                    p1res = "Forty";
-                if (player2point == 1)
-                    p2res = "Fifteen";
-                if (player2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (player2point > player1point && player2point < 4)
-            {
-                if (player2point == 2)
-                    p2res = "Thirty";
-                if (player2point == 3)
-                    p2res = "Forty";
-                if (player1point == 1)
-                    p1res = "Fifteen";
-                if (player1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-
-            if (player1point > player2point && player2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (player2point > player1point && player1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (player1point >= 4 && player2point >= 0 && (player1point - player2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (player2point >= 4 && player1point >= 0 && (player2point - player1point) >= 2)
-            {
-                score = "Win for player2";
+                score = GetDisplayText(player1point) + "-" + GetDisplayText(player2point);
             }
             return score;
+        }
+
+        private string GetDisplayText(int number)
+        {
+            switch (number)
+            {
+                case 0: return "Love";
+                case 1: return "Fifteen";
+                case 2: return "Thirty";
+                case 3: return "Forty";
+                default: throw new Exception("Error number");
+            }
         }
 
         public void SetPlayer1Score(int number)
